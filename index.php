2e698edc724b421ed1a25fbc2bb73380a34fea52
QuinteroAgustin <agustin.quintero@limayrac.fr>
2021-01-15 13:41:10 +0100

    add du index.php et edit fichier.txt

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+<body>
+    <h1>Accueil du web</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati earum laudantium incidunt velit dicta. Laudantium cum non tempora vero esse ipsa tempore dolorum harum. Dolorem repellendus rerum incidunt error recusandae.</p>
+    
+</body>
+</html>
