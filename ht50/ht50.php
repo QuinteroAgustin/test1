e97464ef3104448f54fa4bfea73002684dcce472
QuinteroAgustin <agustin.quintero@limayrac.fr>
2021-01-21 08:07:01 +0100

    ht50 moitie

--- ht50/ht50.php
+++ ht50/ht50.php
@@ -10,9 +10,28 @@
     <h1>ht50 : form</h1>
 
     <form action="verif.php" method="POST">
-        
-    
-        <button>Envoyer</button> <button>Envoyer</button>
+        <label for="nom">Nom :</label><br>
+        <input type="text" name="nom" id="nom">
+        <br>
+        <label for="prenom">Prénom :</label><br>
+        <input type="text" name="prenom" id="prenom">
+        <p>J'aime :</p>
+        <label for="biere">La bière</label>
+        <input type="checkbox" name="biere" id="biere">
+        <br>
+        <label for="aligot">L'aligot</label>
+        <input type="checkbox" name="aligot" id="aligot" selected>
+        <br>
+        <label for="chou">La choucroute</label>
+        <input type="checkbox" name="chou" id="chou">
+        <br>
+        <p>Je suis un/une :</p>
+        <label for="garcon">Garçon</label>
+        <input type="radio" name="g" id="garcon" value="g">
+        <label for="fille">Fille</label>
+        <input type="radio" name="g" id="fille" value="f">        
+        <br>
+        <button>Envoyer</button>&nbsp;<button>Réinitialiser</button>
     </form>
     
     <p>Aller au <a href="#top">Top</a></p>
