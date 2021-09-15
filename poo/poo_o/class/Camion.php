db07d906461899423f637af98b73afe47456f521
QuinteroAgustin <agustin.quintero@limayrac.fr>
2021-09-15 11:49:27 +0200

    zebi

--- poo/poo_o/class/Camion.php
+++ poo/poo_o/class/Camion.php
@@ -27,12 +27,12 @@
         }  
         // Afficher
         function afficher() {
-            echo "<p>--- Description de " . $this->nom . " ---</p>";
+            echo "<p>--- Description de " . $this->get_nom() . " ---</p>";
             echo "<ul>";
-            echo "<li>Marque      : " . $this->marque . "</li>";
-            echo "<li>Modèle      : " . $this->modele . "</li>";
-            echo "<li>Nom         : " . $this->nom . "</li>";
-            echo "<li>Compteur    : " . $this->compteur . "</li>";
+            echo "<li>Marque      : " . $this->get_marque() . "</li>";
+            echo "<li>Modèle      : " . $this->get_modele() . "</li>";
+            echo "<li>Nom         : " . $this->get_nom() . "</li>";
+            echo "<li>Compteur    : " . $this->get_compteur() . "</li>";
             echo "<li>a démarré ? : " . $this->get_lib_etat() . "</li>";
             echo "<li>Type : ". $this->type ."</li>";
             echo "<li>Chargement  : " . $this->get_chargement() . " kg</li>";
