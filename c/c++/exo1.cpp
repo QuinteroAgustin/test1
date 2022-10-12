feb865bff26a597494cf00ddc604aee4415398a3
QuinteroAgustin <agustin.quintero@limayrac.fr>
2022-10-12 16:58:30 +0200

    aze

--- c/c++/exo1.cpp
+++ c/c++/exo1.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main()
 {
     /* code */
-    cout << "Saluuut" << endl;
+    cout << "Bonjour tout le monde !" << endl << "Comment allez vous ?" << endl;
     return 0;
 }
---
+++ c/c++/exo2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /* code */
+    cout << "Bonjour Gérard !" << endl << "Quel âge as-tu ?" << endl << "Joyeux Noël !" << endl << "Où vas-tu ? \" \\ " << endl;
+    return 0;
+}
