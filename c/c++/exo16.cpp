7bea3433d43062adc30008fc00644d7691de0e13
QuinteroAgustin <agustin.quintero@limayrac.fr>
2022-10-13 13:52:47 +0200

    aaa

--- c/c++/exo16.cpp
+++ c/c++/exo16.cpp
@@ -6,16 +6,17 @@ using namespace std;
 int main()
 {
     /* Déclaration des variables */
-    int a(0);
+    int anote(0);
+    double moyenne(0);
 
     // reception des données
-    std::cout << "Entrez un nombre !" << std::endl;
-    cin >> a;
-    for(int i=1; i<=a ; i++){
-        std::cout << "Le carré de " << i <<" est : " << i*i << std::endl;
-    }
+    
+
 
     //affichage des données
+    std::cout << "Note max : "<< noteM << " Apparait : " << noteMa << ' Fois<< std::endl;
+    std::cout << "/* message */" << std::endl;
+    std::cout << "/* message */" << std::endl;
     
     return 0;
 }
