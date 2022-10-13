21ef206ba9bfaaee8373cb2cbde280a843d1f6e3
QuinteroAgustin <agustin.quintero@limayrac.fr>
2022-10-13 09:00:22 +0200

    oklm

--- c/c++/exo2.cpp
+++ c/c++/exo2.cpp
@@ -6,5 +6,5 @@ int main()
 {
     /* code */
     cout << "Bonjour Gérard !" << endl << "Quel âge as-tu ?" << endl << "Joyeux Noël !" << endl << "Où vas-tu ? \" \\ " << endl;
-    return 0;
+    return 1;
 }
