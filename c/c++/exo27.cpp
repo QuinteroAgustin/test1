77c162d03131e5e2f4dcc6c1fcecbd7c263d89b5
QuinteroAgustin <agustin.quintero@limayrac.fr>
2023-02-22 18:19:42 +0100

    ttt

---
+++ c/c++/exo27.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#define string symbole;
+using namespace std ;
+
+int addition(float a, float b){
+    return a+b;
+}
+int soustraction(float a, float b){
+    return a-b;
+}
+int multiplication(float a, float b){
+    return a*b;
+}
+int division(float a, float b){
+    return a/b;
+}
+int modulo(int a, int b){
+    return a%b;
+}
+
+void calcul(int a, int b){
+    float result;
+
+    if(symbole == "-"){
+        result = soustraction(a, b);
+    }else if(symbole == "*"){
+        result = multiplication(a, b);
+    }else if(symbole == "/"){
+        result = division(a, b);
+        if(b==0){
+            std::cout << "Division par zéro impossible !" << std::endl;
+            result = 0;
+        }
+    }else if(symbole == "%"){
+        result = modulo(a, b);
+    }else{
+        result = addition(a, b);
+    }
+    std::cout << "Le résultat de "<< a <<" "<< symbole << " " << b << " = " << result << std::endl;
+}
+
+int main(void){
+    float a, b;
+    string symb;
+    std::cout << "Entrez le nb a : " << std::endl;
+    cin >> a;
+    std::cout << "Entrez le nb b : " << std::endl;
+    cin >> b;
+    std::cout << "Entrez le symbole : " << std::endl;
+    cin >> symb;
+    const symbole = symb;
+
+    calcul(a, b);
+
+    return 0;
+}
